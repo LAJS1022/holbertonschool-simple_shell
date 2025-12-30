5246da6cf29ff82c0e2bd546245e573f081eb1dd
LuisJimenez <luisssajs@gmail.com>
2025-12-30 00:30:36 +0000

    shell 0.3

---
+++ shell.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+/**
+ * run_shell - main loop of the shell
+ */
+void run_shell(void)
+{
+    char *line = NULL, *args[64], *path_cmd = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+
+    while (1)
+    {
+        write(STDOUT_FILENO, ":) ", 3);
+
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+
+        tokenize(line, args);
+
+        if (args[0] == NULL)
+            continue;
+
+        path_cmd = resolve_path(args[0]);
+
+        if (path_cmd != NULL && access(path_cmd, X_OK) == 0)
+        {
+            pid = fork();
+            if (pid == 0)
+            {
+                execve(path_cmd, args, environ);
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+            else if (pid > 0)
+                wait(NULL);
+        }
+        else
+        {
+            write(STDERR_FILENO, args[0], strlen(args[0]));
+            write(STDERR_FILENO, ": command not found\n", 21);
+        }
+
+        free(path_cmd);
+    }
+}
