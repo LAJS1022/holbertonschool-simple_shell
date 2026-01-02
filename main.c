b79120136976530f4a98deea8721d112e618ae03
LuisJimenez <luisssajs@gmail.com>
2026-01-02 22:32:11 +0000

    shell 0.4

--- main.c
+++ main.c
@@ -15,20 +15,3 @@ int main(int argc, char **argv)
     run_shell();
     return (last_status);
 }
-#include "shell.h"
-
-/**
- * main - entry point of the shell
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: exit status of last executed command
- */
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
-
-    run_shell();
-    return (last_status);
-}
